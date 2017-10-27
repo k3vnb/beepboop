794671a9568261114571858ae3d73449039c8e3f
Kevin Boyle <papershack@gmail.com>
2017-10-27 09:57:47 -0700

    control for isNaN entry, send input to string then array

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
+//Business End
+var toBoop = function(integer) {
+
+}
+
+
+
+
 //Front End
 $(document).ready(function() {
   $("form#numbersubmit").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#numnum").val());
-    // var beep = toBoop(userInput);
-    $("#result").append(userInput)
+    if (isNaN(userInput)) {
+      alert("please enter valid number");
+    };
+    console.log(userInput)
+    var blip = userInput.toString();
+    console.log(blip);
+    var beep = blip.split();
+    console.log(beep)
+    // var beep = toBoot(userInput);
+    $("#result").append(userInput);
   });
 });
